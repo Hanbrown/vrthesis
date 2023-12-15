f8d82a8006b18fc194a1c2e3ca6b987366ec5ae6
Pranav Rao <prao2@scu.edu>
2023-12-15 02:04:22 -0600

    Add new scene with gurney and desks

---
+++ Assets/Objects/license.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * AHMED KCHIKICH
+ * https://creativecommons.org/licenses/by/4.0/
+ * Material and file format changes were made to increase compatibility with Unity
+ * https://sketchfab.com/3d-models/office-chair-af2f07d06f6349158c1d24d87f5ceb95
+ */
+
+public class license : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
